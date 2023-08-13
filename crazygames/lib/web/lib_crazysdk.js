24d4d3d054a1c2f03d9b0a563adecc4ec6e4f2d0
Fab <fabianjarrett@gmail.com>
2023-08-13 09:03:52 +0100

    Remove unused functions

--- crazygames/lib/web/lib_crazysdk.js
+++ crazygames/lib/web/lib_crazysdk.js
@@ -2,23 +2,9 @@
 
 var LibCrazyGamesSdk = {
 
-    CrazyGamesSdkJs_init: function () {
+    CrazyGamesSdkJs_gameLoadingStop: function () {
         window.CrazyGames.SDK.game.sdkGameLoadingStop();
     },
-
-    // CrazyGamesSdkJs_initListeners: function () {
-    //     window.crazysdk_jstodef_listeners = {
-    //         adStarted: function () {
-    //             JsToDef.send("CrazyGame_adStared");
-    //         },
-    //         adFinished: function () {
-    //             JsToDef.send("CrazyGame_adFinished");
-    //         },
-    //         adError: function () {
-    //             JsToDef.send("CrazyGame_adError");
-    //         }
-    //     }
-    // },
 
     CrazyGamesSdkJs_requestAd: function (type) {
         const type_string = UTF8ToString(type);
@@ -30,19 +16,6 @@ var LibCrazyGamesSdk = {
         window.CrazyGames.SDK.ad.requestAd(type_string, callbacks);
     },
 
-    // CrazyGamesSdkJs_addEventListeners: function () {
-    //     const crazysdk = window.CrazyGames.CrazySDK.getInstance();
-    //     crazysdk.addEventListener('adStarted', window.crazysdk_jstodef_listeners.adStarted);
-    //     crazysdk.addEventListener('adFinished', window.crazysdk_jstodef_listeners.adFinished);
-    //     crazysdk.addEventListener('adError', window.crazysdk_jstodef_listeners.adError);
-    // },
-    // CrazyGamesSdkJs_clearEventListeners: function () {
-    //     const crazysdk = window.CrazyGames.CrazySDK.getInstance();
-    //     crazysdk.removeEventListener('adStarted', window.crazysdk_jstodef_listeners.adStarted);
-    //     crazysdk.removeEventListener('adFinished', window.crazysdk_jstodef_listeners.adFinished);
-    //     crazysdk.removeEventListener('adError', window.crazysdk_jstodef_listeners.adError);
-    // },
-
     CrazyGamesSdkJs_gameplayStart: function () {
         window.CrazyGames.SDK.game.gameplayStart();
     },
@@ -50,10 +23,10 @@ var LibCrazyGamesSdk = {
     CrazyGamesSdkJs_gameplayStop: function () {
         window.CrazyGames.SDK.game.gameplayStop();
     },
+
     CrazyGamesSdkJs_happyTime: function () {
         window.CrazyGames.SDK.game.happytime();
     }
-
 
 }
 
